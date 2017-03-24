28dfca785bf911fe444f774984bcd0ac9bfdb611
Stefan Walther <Stefan.Walther@qlik.com>
2017-03-25 00:53:46 +0100

    feat(speedy): First runniing prototype

--- docker/speedy/src/index.js
+++ docker/speedy/src/index.js
@@ -1,26 +1,61 @@
 const Influx = require('influx');
 const influx = new Influx.InfluxDB({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    schema: [
-
-    ]
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  schema: [
+    {
+      measurement: 'speed_test',
+      fields: {
+        download: Influx.FieldType.INTEGER,
+        upload: Influx.FieldType.INTEGER
+      },
+      tags: [
+        'foo'
+      ]
+    }
+  ]
 });
 const speedTest = require('speedtest-net');
 const test = speedTest({maxTime: 5000});
 
 console.log('ENV => DB_HOST', process.env.DB_HOST);
 console.log('ENV => DB_PORT', process.env.DB_PORT);
+console.log('ENV => DB_NAME', process.env.DB_NAME);
 
 test.on('data', data => {
-    console.dir(data);
+  console.dir(data);
+
+  influx
+    .writePoints([
+      {
+        measurement: 'speed_test',
+        fields: {
+          download: data.speeds.download,
+          upload: data.speeds.upload
+        },
+        tags: {
+          foo: 'bar'
+        }
+      }
+    ])
+    .then(() => {
+      return influx.query(`
+      select * from speed_test
+      order by time desc
+      limit 10
+    `);
+    })
+    .then(rows => {
+      rows.forEach(row => console.log(`Download: ${row.download} Upload: ${row.upload}`))
+    })
+
 });
 
-test.on('done', data => {
-    // console.log('done');
-    // console.dir(data);
-});
+// test.on('done', data => {
+//     console.log('done');
+//     console.dir(data);
+// });
 
 test.on('error', err => {
-    console.error(err);
+  console.error(err);
 });
