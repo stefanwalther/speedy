2bc457bd2cbf900bee204dea7bfe5aa6a83df879
Stefan Walther <Stefan.Walther@qlik.com>
2017-03-25 01:04:13 +0100

    feat(speedy): Add scheduler

--- docker/speedy/src/index.js
+++ docker/speedy/src/index.js
@@ -16,46 +16,63 @@ const influx = new Influx.InfluxDB({
   ]
 });
 const speedTest = require('speedtest-net');
-const test = speedTest({maxTime: 5000});
+const schedule = require('node-schedule');
 
 console.log('ENV => DB_HOST', process.env.DB_HOST);
 console.log('ENV => DB_PORT', process.env.DB_PORT);
 console.log('ENV => DB_NAME', process.env.DB_NAME);
 
-test.on('data', data => {
-  console.dir(data);
-
-  influx
-    .writePoints([
-      {
-        measurement: 'speed_test',
-        fields: {
-          download: data.speeds.download,
-          upload: data.speeds.upload
-        },
-        tags: {
-          foo: 'bar'
-        }
-      }
-    ])
-    .then(() => {
-      return influx.query(`
-      select * from speed_test
-      order by time desc
-      limit 10
-    `);
-    })
-    .then(rows => {
-      rows.forEach(row => console.log(`Download: ${row.download} Upload: ${row.upload}`))
-    })
-
-});
-
-// test.on('done', data => {
-//     console.log('done');
-//     console.dir(data);
+// run it every minute
+// schedule.scheduleJob('* * * * *', () => {
+//
 // });
 
-test.on('error', err => {
-  console.error(err);
-});
+// run it every 10 seconds
+setInterval(run, 10000);
+
+function run() {
+  const test = speedTest({maxTime: 5000});
+
+  // console.log('Run speed test');
+
+  test.on('data', data => {
+    // console.dir(data);
+
+    influx
+      .writePoints([
+        {
+          measurement: 'speed_test',
+          fields: {
+            download: data.speeds.download,
+            upload: data.speeds.upload
+          },
+          tags: {
+            foo: 'bar'
+          }
+        }
+      ])
+      .then(() => {
+        return influx.query(`
+      select * from speed_test
+      order by time desc
+      limit 1
+    `);
+      })
+      .then(rows => {
+        rows.forEach(row => console.log(`Download: ${row.download} Upload: ${row.upload}`))
+      })
+
+  });
+
+  // test.on('done', data => {
+  //     console.log('done');
+  //     console.dir(data);
+  // });
+
+  test.on('error', err => {
+    console.error(err);
+  });
+
+}
+
+
